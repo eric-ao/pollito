da4e675e177dee2276e49ab07e128bc53b36e2f0
eric-ao <obaya.andres.eric@gmail.com>
2022-05-08 02:20:14 +0200

    Added role creation when joining a guild

--- scripts/roles.js
+++ scripts/roles.js
@@ -2,6 +2,10 @@ const {client} = require('../client')
 const logger = require('../util/logger')
 const config = require('../config.json')
 
+/**
+ * Everytime Pollito is started, it will check the guilds for the required roles.
+ * In case the guild does not have the roles, Pollito will create them.
+ */
 function checkForRoles() {
     let foundRolesCounter = 0;
     let createdRolesCounter = 0;
@@ -13,11 +17,7 @@ function checkForRoles() {
         guildsCounter++;
         let role = guild.roles.cache.find(role => role.name === config.rol_amigo_pollito)
         if(role === undefined) {
-            guild.roles.create({
-                name: config.rol_amigo_pollito,
-                color: [245, 219, 91],
-                mentionable: true
-            })
+            createRole(guild, config.rol_amigo_pollito, config.rol_amigo_pollito_color);
             createdRolesCounter++;
         } else {
             foundRolesCounter++;
@@ -29,5 +29,28 @@ function checkForRoles() {
     logger.print(`Found ${foundRolesCounter} "${config.rol_amigo_pollito}" roles.`)
     logger.print(`Created ${createdRolesCounter} "${config.rol_amigo_pollito}" roles.`)
 }
+
+/**
+ * Creates a role in a guild.
+ * @param guild - the guild where the role is going to be created.
+ * @param role - the name of the role.
+ * @param color - the color for the role.
+ */
+function createRole(guild, role, color) {
+    guild.roles.create({
+        name: role,
+        color: color,
+        mentionable: true
+    })
+    logger.print(`Created the "${role}" role in a guild.`)
+}
+
+/**
+ * Everytime Pollito joins a new guild, it will automatically create the needed roles.
+ */
+client.on('guildCreate', guild => {
+    logger.print(`Pollito just joined a new guild!`);
+    createRole(guild, config.rol_amigo_pollito, config.rol_amigo_pollito_color);
+})
 
 module.exports = { checkForRoles }

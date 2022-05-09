20e093a8e022768809fd8ab25a60fbe9363daeed
eric-ao <obaya.andres.eric@gmail.com>
2022-05-09 15:35:18 +0200

    Added role restrictions to the rolesmsg command

--- commands/rolesMessage.js
+++ commands/rolesMessage.js
@@ -9,23 +9,30 @@ module.exports = {
         .setDescription('Envía un mensaje en el que los usuarios pueden reaccionar para ponerse y quitarse roles'),
 
     async execute(interaction) {
-        //Creates the embed.
-        let embed = new MessageEmbed()
-            .setColor('#ffde4a')
-            .setTitle(config.roles.msg_title)
-            .setDescription(
-                'Reacciona para obtener los roles deseados! \n' +
-                '\n🐣 - Amigo de Pollito (te da los buenos días y las buenas noches)\n\u200B'
-            ).setFooter('Elimina la reacción para quitarte el rol');
+        //If the user doesn't have the Admin Pollito role, they can't use the command.
+        if(interaction.member.roles.cache.find(role => role.name === config.roles.admin_pollito.name) === undefined)
+            interaction.reply("No tienes permisos.").then(() => {
+                setTimeout(() => interaction.deleteReply(), 3000);
+            }).catch(err => logger.error(err))
+        else {
+            //Creates the embed.
+            let embed = new MessageEmbed()
+                .setColor('#ffde4a')
+                .setTitle(config.roles.msg_title)
+                .setDescription(
+                    'Reacciona para obtener los roles deseados! \n' +
+                    '\n🐣 - Amigo de Pollito (te da los buenos días y las buenas noches)\n\u200B'
+                ).setFooter('Elimina la reacción para quitarte el rol');
 
-        //Sends the embed and adds the reaction emojis.
-        await interaction.channel.send({embeds: [embed]}).then(msg => {
-            msg.react(config.roles.amigo_pollito.emoji)
-        }).catch(err => logger.error(err));
+            //Sends the embed and adds the reaction emojis.
+            await interaction.channel.send({embeds: [embed]}).then(msg => {
+                msg.react(config.roles.amigo_pollito.emoji)
+                }).catch(err => logger.error(err));
 
-        //Replies to the command and deletes the reply after 3 seconds.
-        interaction.reply("Mensaje enviado!").then(() => {
-            setTimeout(() => interaction.deleteReply(), 3000);
-        }).catch(err => logger.error(err))
+            //Replies to the command and deletes the reply after 3 seconds.
+            interaction.reply("Mensaje enviado!").then(() => {
+                setTimeout(() => interaction.deleteReply(), 3000);
+                }).catch(err => logger.error(err))
+        }
     }
 }

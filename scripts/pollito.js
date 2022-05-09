d132af0bbb33858f38e912afd2c7f301c1090b1d
eric-ao <obaya.andres.eric@gmail.com>
2022-05-09 22:17:00 +0200

    Refactorized code to reduce duplicated code

--- scripts/pollito.js
+++ scripts/pollito.js
@@ -4,59 +4,54 @@ const config = require('../config.json')
 const lang = require('../lang/es.json')
 const {client} = require('../client')
 
-//Schedules the event to wish a good day.
-//It will only message those users with the "Amigo de pollito" role.
-async function scheduleGM() {
-    logger.print(`Scheduling wishing a good day at ${config.good_morning_hour}:00...`)
-    let scheduleRule = new schedule.RecurrenceRule();
-    scheduleRule.hour = config.good_morning_hour;
-    scheduleRule.tz = 'Etc/GMT+2'
-    schedule.scheduleJob(scheduleRule, async () => {
-        let counter = 0;
-        let msg = `Pío pío, buenos dias! ${GMmsgs[Math.floor(Math.random()*GMmsgs.length)]} 🐥`;
-        let idsDone = [];
-        client.guilds.cache.forEach(guild => {
-            guild.members.cache.filter(member => member.roles.cache.find(role => role.name === config.roles.amigo_pollito.name))
-                .forEach(member => {
-                    if(!idsDone.includes(member.id)) {
-                        counter++;
-                        idsDone.push(member.id);
-                        member.send(msg)
-                            .catch(err => logger.error(err));
-                    }            
-                })
-        });
-        logger.print(`Wished a good day to ${counter} users.`)
-    })
-}
-//Schedules the event to wish a good night.
-//It will only message those users with the "Amigo de pollito" role.
-async function scheduleGN() {
-    logger.print(`Scheduling wishing a good night at ${config.good_night_hour}:00...`)
-    let scheduleRule = new schedule.RecurrenceRule();
-    scheduleRule.hour = config.good_night_hour;
-    scheduleRule.tz = 'Etc/GMT+2'
-    schedule.scheduleJob(scheduleRule, async () => {
-        let counter = 0;
-        let msg = `Pío pío, buenas noches! ${GNmsgs[Math.floor(Math.random()*GNmsgs.length)]} Te quiero mucho, descansa 💤`
-        let idsDone = [];
-        client.guilds.cache.forEach(guild => {
-            guild.members.cache.filter(member => member.roles.cache.find(role => role.name === config.roles.amigo_pollito.name))
-                .forEach(member => {
-                    if(!idsDone.includes(member.id)) {
-                        counter++;
-                        idsDone.push(member.id);
-                        member.send(msg)
-                            .catch(err => logger.error(err));
-                    }
-                })
-        });
-        logger.print(`Wished a good night to ${counter} users.`)
-    })
-}
-
-
 let GMmsgs = lang.good_morning_msgs;
 let GNmsgs = lang.good_night_msgs;
+
+let scheduleRule = new schedule.RecurrenceRule();
+scheduleRule.tz = 'Etc/GMT+2'
+
+
+/**
+ * Schedules the event to wish a good day.
+ * It will only message those users with the "Amigo de pollito" role.
+ */
+async function scheduleGM() {
+    logger.print(`Scheduling wishing a good day at ${config.good_morning_hour}:00...`)
+    scheduleRule.hour = config.good_morning_hour;
+    schedule.scheduleJob(scheduleRule, scheduledJob(`Pío pío, buenos dias! ${GMmsgs[Math.floor(Math.random()*GMmsgs.length)]} 🐥`, "day"));
+}
+
+/**
+ * Schedules the event to wish a good night.
+ * It will only message those users with the "Amigo de pollito" role.
+ */
+async function scheduleGN() {
+    logger.print(`Scheduling wishing a good night at ${config.good_night_hour}:00...`)
+    scheduleRule.hour = config.good_night_hour;
+    schedule.scheduleJob(scheduleRule, scheduledJob(`Pío pío, buenas noches! ${GNmsgs[Math.floor(Math.random()*GNmsgs.length)]} Te quiero mucho, descansa 💤`, "night"));
+}
+
+/**
+ * Gets every guild member with the role "Amigo de Pollito" and sends them a message.
+ * @param message - message that is gonna be send to the users. 
+ * @param time - time of the day of the job.
+ */
+async function scheduledJob(message, time) {
+    let counter = 0;
+    let idsDone = [];
+    client.guilds.cache.forEach(guild => {
+        guild.members.cache.filter(member => member.roles.cache.find(role => role.name === config.roles.amigo_pollito.name))
+            .forEach(member => {
+                if(!idsDone.includes(member.id)) {
+                    counter++;
+                    idsDone.push(member.id);
+                    member.send(message)
+                        .catch(err => logger.error(err));
+                }
+            })
+    });
+    logger.print(`Wished a good ${time} to ${counter} users.`)
+}
+
 
 module.exports = { scheduleGM, scheduleGN }
--- scripts/roles.js
+++ scripts/roles.js
@@ -2,6 +2,26 @@ const {client} = require('../client')
 const logger = require('../util/logger')
 const config = require('../config.json')
 
+
+/**
+ * Checks if a guild has certain role, if it doesn't, the role is created.
+ * @param guild - where to check 
+ * @param roleName - name of the role to look for
+ * @param roleColor - color of the role, in case it needs to be created.
+ * @returns true in case its found, false otherwise
+ */
+async function findORcreate(guild, roleName, roleColor) {
+    let role = guild.roles.cache.find(role => role.name === roleName)
+    if(role === undefined) {
+        logger.print(`${roleName} role not found, creating one...`)
+        await createRole(guild, roleName, roleColor);
+        return false;
+    } else {
+        logger.print(`${roleName} role found in the new guild!`)
+        return true;
+    }
+}
+
 /**
  * Everytime Pollito is started, it will check the guilds for the required roles.
  * In case the guild does not have the roles, Pollito will create them.
@@ -18,21 +38,11 @@ function checkForRoles() {
     client.guilds.cache.forEach(guild => {
         guildsCounter++;
         
-        let role = guild.roles.cache.find(role => role.name === config.roles.amigo_pollito.name)
-        if(role === undefined) {
-            createRole(guild, config.roles.amigo_pollito.name, config.roles.amigo_pollito.color);
-            createdAmigoRolesCounter++;
-        } else {
-            foundAmigoRolesCounter++;
-        }
+        if(findORcreate(guild, config.roles.amigo_pollito.name, config.roles.amigo_pollito.color)) foundAmigoRolesCounter++;
+        else createdAmigoRolesCounter++;
 
-        role = guild.roles.cache.find(role => role.name === config.roles.admin_pollito.name)
-        if(role === undefined) {
-            createRole(guild, config.roles.admin_pollito.name, config.roles.admin_pollito.color);
-            createdAdminRolesCounter++;
-        } else {
-            foundAdminRolesCounter++;
-        }
+        if(findORcreate(guild, config.roles.admin_pollito.name, config.roles.admin_pollito.color)) foundAdminRolesCounter++;
+        else createdAdminRolesCounter++;
     })
 
     logger.print(`Checking done!`)
@@ -64,22 +74,10 @@ client.on('guildCreate', guild => {
     logger.print(`Pollito just joined a new guild!`);
     
     logger.print(`Checking for the ${config.roles.amigo_pollito.name} role...`)
-    let role = guild.roles.cache.find(role => role.name === config.roles.amigo_pollito.name)
-    if(role === undefined) {
-        logger.print(`${config.roles.amigo_pollito.name} role not found, creating one...`)
-        createRole(guild, config.roles.amigo_pollito.name, config.roles.amigo_pollito.color);
-    } else {
-        logger.print(`${config.roles.amigo_pollito.name} role found in the new guild!`)
-    }
+    findORcreate(guild, config.roles.amigo_pollito.name, config.roles.amigo_pollito.color)
 
     logger.print(`Checking for the ${config.roles.admin_pollito.name} role...`)
-    role = guild.roles.cache.find(role => role.name === config.roles.admin_pollito.name)
-    if(role === undefined) {
-        logger.print(`${config.roles.admin_pollito.name} role not found, creating one...`)
-        createRole(guild, config.roles.admin_pollito.name, config.roles.admin_pollito.color);
-    } else {
-        logger.print(`${config.roles.admin_pollito.name} role found in the new guild!`)
-    }
+    findORcreate(guild, config.roles.admin_pollito.name, config.roles.admin_pollito.color)
 })
 
 //Everytime someone reacts to a message, if the message is the role assignation one, gives the role.
@@ -104,11 +102,7 @@ client.on('messageReactionAdd', async (reaction, user) => {
     if(reacMsg.author.id === client.user.id) {
         if(reacMsg.embeds[0] !== undefined && reacMsg.embeds[0].title === config.roles.msg_title) {
             if(reacEmoji === apEmoji) {
-                let role = reacMsg.guild.roles.cache.find(role => role.name === apName)
-                if(role === undefined) {
-                    role = await createRole(reacMsg.guild, apName, apColor)
-                    logger.print(`Created the "${role.name}" role in a guild.`)
-                }
+                findORcreate(guild, apName, apColor)
                 await reacMsg.guild.members.cache.get(user.id).roles.add(role.id);
                 logger.print(`Role "${role.name}" added to a user.`)
             }

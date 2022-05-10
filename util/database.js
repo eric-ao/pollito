1818cec97c8cedef9bd6b3c5bd3422023bb8a002
eric-ao <obaya.andres.eric@gmail.com>
2022-05-10 14:49:58 +0200

    Fixed connection to database

--- util/database.js
+++ util/database.js
@@ -3,7 +3,7 @@ let logger = require('./logger');
 require('dotenv').config();
 
 let connection = mysql.createConnection({
-    host: 'localhost',
+    host: process.env.DB_HOST,
     user: process.env.DB_USER,
     password: process.env.DB_PASS,
     database: process.env.DB_NAME

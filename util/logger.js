7eef55c9a9105b3c3ddefa4093f363e0b85a8dd3
eric-ao <obaya.andres.eric@gmail.com>
2022-05-31 02:18:51 +0200

    Now the folder with the logs is created automatically

--- util/logger.js
+++ util/logger.js
@@ -1,4 +1,6 @@
 const fs = require('fs');
+const dirPath = './logs';
+
 
 /**
  * Sends through console a new info message.
@@ -7,7 +9,15 @@ const fs = require('fs');
 function print(message) {
     let msg = `${getTimeStamp()} - ${message}`;
     console.log(msg);
-    fs.writeFile(getPath(), msg+"\n", {flag: 'a'}, err => {if (err) error(err)})
+    try {
+        if(!fs.existsSync(dirPath)) 
+            fs.mkdir(dirPath, (err) => {
+                if(err) error(err);
+            });
+        fs.writeFile(getPath(), msg+"\n", {flag: 'a', recursive: true}, err => {if (err) error(err)})
+    } catch (err) {
+        error(err);
+    }
 }
 
 /**
@@ -17,7 +27,15 @@ function print(message) {
 function error(message) {
     let msg = `${getTimeStamp()} - ERROR: ${message}`;
     console.log(msg);
-    fs.writeFile(getPath(), msg+"\n", {flag: 'a'}, err => {if (err) console.error(err)})
+    try {
+        if(!fs.existsSync(dirPath)) 
+            fs.mkdir(dirPath, (err) => {
+                if(err) console.error(err);
+            });
+        fs.writeFile(getPath(), msg+"\n", {flag: 'a', recursive: true}, err => {if (err) console.error(err)})
+    }catch (err) {
+        console.error(err);
+    }
 }
 
 /**
@@ -32,7 +50,7 @@ function getTimeStamp() {
 
 function getPath() {
     let date = new Date();
-    return `./logs/${date.getDate()}-${date.getMonth()}-${date.getFullYear()}.log`
+    return `${dirPath}/${date.getDate()}-${date.getMonth()}-${date.getFullYear()}.log`
 }
 
 module.exports = { print, error }

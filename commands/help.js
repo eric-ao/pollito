1bdb7f5c136d18a514d6789ae815c2d0ab5ff206
eric-ao <obaya.andres.eric@gmail.com>
2022-07-01 02:18:48 +0200

    Added more info to the help command

--- commands/help.js
+++ commands/help.js
@@ -8,7 +8,6 @@ module.exports = {
         .setDescription('Comandos útiles de Pollito'),
 
     async execute(interaction) {
-        let eggs = await database.getHuevos(interaction.user.id);
         let embed = new MessageEmbed()
                 .setColor('#ffde4a')
                 .setTitle("Comandos")
@@ -16,7 +15,10 @@ module.exports = {
                     '· **/birthday** - Pollito te felicitará tu cumpleaños\n' + 
                     '· **/rolesmsg** - Mensaje para el rol de *Amigo de Pollito*\n' +
                     '· **/catalina** - Cuantas veces se ha cambiado la foto Catalina\n' +
-                    '· **/huevos** - Comprueba cuantos tienes')
-        interaction.reply({embeds: [embed]})
+                    '· **/huevos** - Comprueba cuantos tienes\n\u200B\n'+
+                    'Si eres *Amigo de Pollito*, te dirá buenos días y buenas noches todos los días!')
+        interaction.reply({embeds: [embed]}).then(() => {
+            setTimeout(() => interaction.deleteReply(), 15000);
+        }).catch(err => logger.error(err));
     }
 }
